53dc97ce9d95fd1c9c4ccdd2d82c8a0208814c27
rshuken <ryanshuken@gmail.com>
2020-10-07 10:41:06 -0600

    got the thing working

--- src/Components/SideBar/SideBar.js
+++ src/Components/SideBar/SideBar.js
@@ -1,13 +1,17 @@
 import React, { Component } from "react";
 import { NavLink, withRouter } from "react-router-dom";
+import AppContext from '../../AppContext';
 
- class SideBar extends Component {
+ class SideBar extends Component {  
+
+  static contextType = AppContext; 
+
   render() {
-
-    const template = this.props.folders ? (
+    console.log('this is the context', this.context);
+    const template = this.context.folders ? (
       <div className="sidebar">
         <ul>Folders</ul>
-        {this.props.folders.map((folder) => (
+        {this.context.folders.map((folder) => (
           <NavLink key={folder.id} to={{ pathname: `/folder/${folder.id}` }}>
             <li>{folder.name}</li>
           </NavLink>
